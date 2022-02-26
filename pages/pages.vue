8bcd1f685e615f982015448fd3ec5da5a3a3e8dc
Shotaro K <26667526+shokm@users.noreply.github.com>
2022-02-26 20:39:53 +0900

    [add] 固定ページの一覧ページ作成

---
+++ pages/pages.vue
@@ -0,0 +1,91 @@
+<template>
+  <div>
+    <div class="background_pink" />
+    <div class="background_lightBlue" />
+    <div class="background_blue" />
+    <nav>
+      <NuxtLink to="/">
+        <h1><img id="logo" src="~assets/svg/logo.svg" alt="東京都市大学デジタルコンテンツ研究会"></h1>
+      </NuxtLink>
+      <ul>
+        <li>
+          <NuxtLink :to="'/' + top.nav.link1.id">
+            {{ top.nav.link1.title }}<br>
+            <span class="sub">{{ top.nav.link1.id }}</span>
+          </NuxtLink>
+        </li>
+        <li>
+          <NuxtLink :to="'/' + top.nav.link2.id">
+            {{ top.nav.link2.title }}<br>
+            <span class="sub">{{ top.nav.link2.id }}</span>
+          </NuxtLink>
+        </li>
+        <li>
+          <NuxtLink :to="'/' + top.nav.link3.id">
+            {{ top.nav.link3.title }}<br>
+            <span class="sub">{{ top.nav.link3.id }}</span>
+          </NuxtLink>
+        </li>
+      </ul>
+      <NuxtLink :to="'/' + top.nav.joinus.id" class="button">
+        {{ top.nav.joinus.title }}
+      </NuxtLink>
+    </nav>
+    <main>
+      <div id="blogWrapper">
+        <div id="blogIndex" class="link-decoration">
+          <p />
+          <h1>固定ページ一覧</h1>
+          <NuxtLink v-for="i in pageall" :key="i.id" :to="'/' + i.id" class="blogall_box">
+            <p class="blogall_date">
+              {{ new Date(i.publishedAt).toLocaleDateString() }}
+            </p>
+            <p class="blogall_title">
+              {{ i.title }}
+            </p>
+          </NuxtLink>
+          <p />
+        </div>
+        <div id="blogSideBar">
+          <h3>Author</h3>
+          <div id="blogAuthor">
+            <div id="blogIcon">
+              <img src="~assets/svg/icon.svg" alt="アイコン">
+            </div>
+            <p>デジタルコンテンツ研究会</p>
+          </div>
+        </div>
+      </div>
+    </main>
+    <footer v-html="top.footer.body" />
+  </div>
+</template>
+<script>
+import axios from 'axios'
+export default {
+  async asyncData () {
+    const res = await Promise.all([
+      axios.get(
+        'https://tcu-dc.microcms.io/api/v1/top',
+        { headers: { 'X-MICROCMS-API-KEY': 'cce632607ed24373acc3e0ba0be10e180d71' } }
+      ),
+      axios.get(
+        'https://tcu-dc.microcms.io/api/v1/page/',
+        { headers: { 'X-MICROCMS-API-KEY': 'cce632607ed24373acc3e0ba0be10e180d71' } }
+      )
+    ])
+    return {
+      top: res[0].data,
+      pageall: res[1].data.contents
+    }
+  },
+  head () {
+    return {
+      title: '固定ページ一覧',
+      meta: [
+        { hid: 'robots', name: 'robots', content: 'noindex, follow' }
+      ]
+    }
+  }
+}
+</script>
